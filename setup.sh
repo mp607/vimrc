a336ac3921843b65045e47b08b59190da479662b
mp607 <mickey60711@gmail.com>
2018-02-07 16:20:55 +0800

    [setup.sh] auto login in github for gist when setup

--- setup.sh
+++ setup.sh
@@ -7,5 +7,10 @@ vim +PluginInstall +qall
 read -p "Please input your github username [default: $LOGNAME]: " GITHUBUSER
 [ -z $GITHUBUSER ] && GITHUBUSER=$LOGNAME
 git config --global github.user $GITHUBUSER
+echo "github.user is $GITHUBUSER now."
+
+# Login in github for Gist
+read -p "Login in github for Gist now? (Needed gist) (Y/n): " LOGIN
+[ "${LOGIN}" == "Y" -o "${LOGIN}" == "y" ] && gist --login
 
 echo "Vimrc is setting down. Enjoy it!"
